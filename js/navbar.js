cf84c9c7a9a53d42cfe835d0482b251dc31ffb55
TorbsT <torbst2001@gmail.com>
2020-10-11 16:10:50 +0200

    Pretty much finished navbar, I think

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,18 @@
 // LAST EDITED BY:
-// Torbjørn, 05.10.2020
+// Torbjørn, 11.10.2020
+// - Pretty much finished the navbar, I think
 
 // ========== GLOBAL VARIABLES ==========
 // Config
-var bannerHeight = 60;
+// NOTE: Navbar logo size is determined by bannerHeight, navBarHeight and imageCellWidthWeight.
+// Tweak these numbers to adjust logo size in different situations.
+var bannerHeight = 70;
 var navBarHeight = 80;
-var minimizedImageHeight = 1.25;  // as a fraction of navBarHeight
-var imageCellWidthWeight = 0.5;  // Button cells have a weight of 1
+var imageCellWidthWeight = 0.65;  // Cell widths get distributed among items. Image cells get this weight compared to buttons, which get 1 weight.
 
 var bodyEL = document.querySelector("body");
-var bannerEL = null;
-var navBarEL = null;
-//var itemTableEL;  // The links are inside of a table with 1 row
-//var itemRowEL;
+var bannerEL = null;  // bannerEL is the spruce wood-part
+var navBarEL = null;  // navBarEL is the orange part with all the buttons and logo
 var navItems =
 [  // Template to use when creating navButtons
     {"display":"Home", "link":"index.html", "displayImg":"../imgs/OBurger-logo-cropped.png"},
@@ -31,14 +31,7 @@ function ResetAll()
     navBarEL = CreateNavBar();
 
     navBarEL.innerHTML = "";
-    /*
-    itemTableEL = document.createElement("table");
-    itemRowEL = itemTableEL.insertRow();
-    */
     CreateNavItems();
-    /*
-    navBarEL.appendChild(itemTableEL);
-    */
     document.addEventListener("scroll", UpdateNavbar);
     window.addEventListener("resize", UpdateNavbar);
     ResetStyle();
@@ -57,7 +50,6 @@ function CreateNavBar()
         // Delete previous navBar.
         navBarEL.parentNode.removeChild(navBarEL);
     }
-
     tempEL = document.createElement("div");
     tempEL.id = "navBar";
     GetBanner().appendChild(tempEL);
@@ -85,11 +77,12 @@ function CreateNavItems()
         var thisItem = navItems[i];
         var thisDisplay = thisItem["display"];
         var thisLink = thisItem["link"];
-        var itemWidth = 100/weightSum;
+        var itemWidth = 1/weightSum;
 
         if ("displayImg" in thisItem)
         {  // Display an image instead of text
             var itemEL = document.createElement("img");
+            itemWidth *= imageCellWidthWeight;
             itemEL.className = "navImg";
             // Images need to be loaded in order to get their width and height,
             // which is necessary for the page to look alright.
@@ -108,8 +101,7 @@ function CreateNavItems()
             console.log(itemWidth);
         }
 
-        itemCellEL.style.width = itemWidth+"%";
-        itemCellEL.style.height = navBarHeight+"px";
+        itemCellEL.relWidth = itemWidth;  // Fraction of window space the cell/button should take
         itemEL.link = thisLink;
         itemEL.addEventListener("click", NavItemClicked);
 
@@ -123,7 +115,11 @@ function GetNavItemWeightSum()
     for (var i = 0; i < navItems.length; i++)
     {
         var item = navItems[i];
-        if ("img" in item) sum += logoCellWidthWeight;
+        if ("displayImg" in item)
+        {
+            sum += imageCellWidthWeight;
+            console.log("hjup");
+        }
         else sum++;
     }
     return sum;
@@ -147,29 +143,63 @@ function ResetStyle()
 function UpdateNavbar()
 {
     var scroll = document.scrollingElement.scrollTop;
+    const vw = document.documentElement.clientWidth;
     // y is a scalar [0, 1]. 0 => navbar is at top, 1 => is as low as possible
     var y = (bannerHeight-scroll)/bannerHeight;
     if (y < 0) y = 0;
 
     navBarEL.style.top = y*bannerHeight+"px";
-    var navImgELs = document.getElementsByClassName("navImg");
-    for (var i = 0; i < navImgELs.length; i++)
-    {
-        var imgEL = navImgELs[i];
-        if (imgEL.naturalWidth == 0) return;  // avoid dividing by zero
-        // imgDimensionRelation is the relation between the source image height to width
-        var imgDimensionRelation = imgEL.naturalHeight/imgEL.naturalWidth;
 
-        var minHeight = minimizedImageHeight;
-        var minWidth = Math.floor(minHeight/imgDimensionRelation);
-        var maxHeight = y*bannerHeight*2+navBarHeight;
-        var maxWidth = Math.floor(maxHeight/imgDimensionRelation);
-        var width = Math.floor(minWidth*(1-y)+maxWidth*y);
-        imgEL.style.width = width+"px";
-        imgEL.style.height = width*imgDimensionRelation+"px";
-        imgEL.style.top = -y*bannerHeight+"px";
+    // navCell handling
+    var navCellELs = document.getElementsByClassName("navCell");
+    for (var i = 0; i < navCellELs.length; i++)
+    {
+        var cellEL = navCellELs[i];
+        var relWidth = cellEL.relWidth;
+        var absWidth = Math.floor(relWidth*vw);  // floor because if it rounds up, the last button may go on a new line
+        cellEL.style.width = absWidth+"px";
+
+        var itemEL = cellEL.firstChild;
+        if (itemEL.classList.contains("navImg"))
+        {
+            var navImgEL = itemEL;  // Better variable name
+            // Item is a navImg. Special image rules apply
+
+            /* RULES FOR NAVIMG APPEARANCE.
+            ALL MUST BE FULFILLED
+            1: The navimg center position is static relative to the navbar.
+            2: The navimg width cannot be larger than its cell-width
+            3: All of the navimg should be visible at all times (restriction to height)
+            4: Following these rules, the navimg should be as big as possible.
+            */
+
+            var rule2Width = absWidth;
+
+
+            var centerX = Math.floor(absWidth/2);
+            var centerY = Math.floor(navBarHeight/2);
+            var rule3Height = (centerY+y*bannerHeight)*2;  // Distance from center of image to top of page, times 2
+            var rule3Width = rule3Height/GetImgHeightWidthRelation(navImgEL);
+
+            var actualWidth = Math.min(rule2Width, rule3Width);
+            console.log("chose "+actualWidth);
+            var actualHeight = Math.floor(actualWidth*GetImgHeightWidthRelation(navImgEL));
+
+            navImgEL.style.width = actualWidth+"px";
+            navImgEL.style.height = actualHeight+"px";
+            console.log(actualHeight+", "+navImgEL.style.height);
+            navImgEL.style.top = Math.floor(centerY-actualHeight/2)+"px";
+            navImgEL.style.left = Math.floor(centerX-actualWidth/2)+"px";
+        }
     }
 }
+function GetImgHeightWidthRelation(imgEL)
+{
+    if (imgEL.naturalWidth == 0) return 0;  // avoid dividing by zero
+    // imgDimensionRelation is the relation between the source image height to width
+    var imgDimensionRelation = imgEL.naturalHeight/imgEL.naturalWidth;
+    return imgDimensionRelation;
+}   
 
 // ========== INITIALIZATION ==========
 ResetAll();
