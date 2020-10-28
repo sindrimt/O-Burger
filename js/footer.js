b9a94449e3ca9cbd8295119778aadf58105f69c3
Marcus Stephan Nordal <marcusnordal@icloud.com>
2020-10-28 17:48:09 +0100

    endringer på footer

--- js/footer.js
+++ js/footer.js
@@ -1,19 +1,16 @@
 // HTML-kode gjort om til js ved å gjøre om alt til en string.
 var footer = '<div id="container">'+
 '        <div id="content">'+
-'            <h1 id="hello">"----- Navbar -----"</h1>'+
-'            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio sunt velit hic numquam, quam cupiditate'+
-'                est fuga, aliquid eveniet dicta et quaerat ea fugiat recusandae porro placeat? Corrupti, tempore id.</p>'+
 '        </div>'+
 ''+
 '        <div id="footer">'+
-'            <a href="#hello"><img src="../O-Burger/imgs/OBurger-logo3.png" alt="Logo" id="footerLogo"></a>'+
-'            <img src="../O-Burger/imgs/banner.jpg" alt="Background" id="footerBackground">'+
+'            <a href="#banner"><img src="../imgs/OBurger-logo3.png" alt="Logo" id="footerLogo"></a>'+
+'            <img src="../imgs/banner.jpg" alt="Background" id="footerBackground">'+
 ''+
-'            <a href=https://wwww.facebook.com/barackobama target="_blank"><img src="../O-Burger/imgs/facebook.png"'+
+'            <a href=https://wwww.facebook.com/barackobama target="_blank"><img src="../imgs/facebook.png"'+
 '                    alt="Facebook" id="facebook"></a>'+
 '            <a href=https://www.instagram.com/barackobama/?hl=en target="_blank"><img'+
-'                    src="../O-Burger/imgs/instagram.png" alt="Instagram" id="instagram"></a>'+
+'                    src="../imgs/instagram.png" alt="Instagram" id="instagram"></a>'+
 ''+
 '        </div>'+
 '    </div>';
