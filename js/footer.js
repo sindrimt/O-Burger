5c5fceb53683fc32fdac057428949b6a32aaa0ec
TorbsT <37838405+TorbsT@users.noreply.github.com>
2020-11-03 01:20:39 +0100

    alotta good shit

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@
 var footer = 
 '        <div id="footer">'+
 '            <a href="#banner"><img src="../imgs/global/OBurger-logo.png" alt="Logo" id="footerLogo"></a>'+
-'            <img src="../imgs/global/sprucewood.jpg" alt="Background" id="footerBackground">'+
+'            <img src="../imgs/global/sprucewood.jpg" alt="Background" id="footerBackground" class="centered">'+
 ''+
 '            <a href=https://wwww.facebook.com/barackobama target="_blank"><img src="../imgs/global/facebook.png"'+
 '                    alt="Facebook" id="facebook"></a>'+
