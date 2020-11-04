7b844e1087ff8f98faf6aebef15059ebfe727762
TorbsT <37838405+TorbsT@users.noreply.github.com>
2020-11-04 02:51:25 +0100

    Completely rewrote footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,47 @@
-// HTML-kode gjort om til js ved å gjøre om alt til en string
-var footer = 
-'        <div id="footer">'+
-'            <a href="#banner"><img src="../imgs/global/OBurger-logo.png" alt="Logo" id="footerLogo"></a>'+
-'            <img src="../imgs/global/sprucewood.jpg" alt="Background" id="footerBackground" class="centered">'+
-''+
-'            <a href=https://wwww.facebook.com/barackobama target="_blank"><img src="../imgs/global/facebook.png"'+
-'                    alt="Facebook" id="facebook"></a>'+
-'            <a href=https://www.instagram.com/barackobama/?hl=en target="_blank"><img'+
-'                    src="../imgs/global/instagram.png" alt="Instagram" id="instagram"></a>'+
-'            <a href=https://docs.google.com/forms/d/e/1FAIpQLSe4xSufUl9toPvV6qXgf01FXZROUV0ViQZG0j13s4xAI2IMzQ/viewform target="_blank"><div'+
-'                    id="review">Leave feedback (google forms)</div></a>'+
-''+
-'        </div>';
+function CreateWrapperFor(element, href, newPage = false)
+{  // Lets an input element have a link to send the user to
+    // Does this by creating a wrapper element of type "a".
+    // The wrapper element is then appended to the footer.
+    var wrapperEL = document.createElement("a");
+    wrapperEL.href = href;
+    if (newPage) wrapperEL.target = "_blank";
+    wrapperEL.appendChild(element);
+    footerEL.appendChild(wrapperEL);
+}
 
-document.write(footer);
+var bodyEL = document.body;
+var footerEL = document.createElement("div");
+footerEL.id = "footer";
+
+var logoEL = document.createElement("img");
+logoEL.src = "../imgs/global/OBurger-logo.png";
+logoEL.alt = "Logo";
+logoEL.id = "footerLogo";
+CreateWrapperFor(logoEL, "#banner", false);
+
+var backgroundEL = document.createElement("img");
+backgroundEL.src = "../imgs/global/sprucewood.jpg";
+backgroundEL.alt = "Background";
+backgroundEL.id = "footerBackground";
+backgroundEL.className = "centered";  // centers the background so it looks cooler and epicer and gooder when zooming or resizing window
+footerEL.appendChild(backgroundEL);
+
+var facebookEL = document.createElement("img");
+facebookEL.src = "../imgs/global/facebook.png";
+facebookEL.alt = "Facebook";
+facebookEL.id = "facebook";
+CreateWrapperFor(facebookEL, "https://wwww.facebook.com/barackobama", true);
+
+var instagramEL = document.createElement("img");
+instagramEL.src = "../imgs/global/instagram.png";
+instagramEL.alt = "Instagram";
+instagramEL.id = "instagram";
+CreateWrapperFor(instagramEL, "https://www.instagram.com/barackobama/?hl=en", true);
+
+var feedbackEL = document.createElement("div");
+feedbackEL.id = "review";
+feedbackEL.innerHTML = "Leave feedback (google forms)";
+CreateWrapperFor(feedbackEL, "https://docs.google.com/forms/d/e/1FAIpQLSe4xSufUl9toPvV6qXgf01FXZROUV0ViQZG0j13s4xAI2IMzQ/viewform", true);
+
+
+bodyEL.appendChild(footerEL);
